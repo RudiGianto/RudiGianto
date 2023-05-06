da861905dd138ab55aeaaf3c182c9cb29a9bd582
Gianto <132178091+RudiGianto@users.noreply.github.com>
2023-05-06 08:27:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+Nederland = 'Gullit'
+Nederland = 'van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = 'Ruud Gullit ' + str(32) + ', Marco van Basten ' + str(54)
+report = f'Ruud Gullit scored in the {goal_0}nd minute\nMarco van Basten scored in the {goal_1}th minute' 
+print(report)
+player ='Ruud Gullit'
+first_name = (player[0:4])  # https://www.w3schools.com/python/python_strings_slicing.asp
+last_name_len = len(player[5:11])  #https://www.w3schools.com/python/ref_func_len.asp
+name_short = 'R. Gullit'
+chant = ('Ruud! ')*3 + ('Ruud!')*1
+good_chant = (2!=3) 
+print(first_name)
+print(last_name_len)
+print(name_short)
+print(chant)
+print(good_chant)
