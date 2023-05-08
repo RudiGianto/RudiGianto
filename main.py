4017a391d4402c0fb147bb5f0bb832d1032421a7
Gianto <132178091+RudiGianto@users.noreply.github.com>
2023-05-08 17:13:20 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # Do not modify these lines
-__winc_id__ = '62311a1767294e058dc13c953e8690a4'
-__human_name__ = 'casting'
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
 
 # Add your code after this line
-leek_price = 2
-a = 'Leek is ' + str(leek_price) + ' euro per kilo.' # #https://www.w3schools.com/python/ref_func_len.asp
-print(a)
-leek_4 = 4
-sum_total = str(leek_price * leek_4)
-print(sum_total)
-broccoli_price = 2.34
-broccoli_order = 1.6
-broccoli_total = str(broccoli_price * broccoli_order)
-x = round(3.7439999999999998, 2)
-broccoli_cost = str(broccoli_order) + 'kg broccoli costs ' + str(x)+'e'
-print(broccoli_cost)
+Nederland = 'Gullit'
+Nederland = 'van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = 'Ruud Gullit ' + str(32) + ', Marco van Basten ' + str(54)
+report = f'Ruud Gullit scored in the {goal_0}nd minute\nMarco van Basten scored in the {goal_1}th minute' 
+print(report)
+player ='Ruud Gullit'
+first_name = (player[0:4])  # https://www.w3schools.com/python/python_strings_slicing.asp
+last_name_len = len(player[5:11])  #https://www.w3schools.com/python/ref_func_len.asp
+name_short = 'R. Gullit'
+chant = ('Ruud! ')*3 + ('Ruud!')*1
+good_chant = (2!=3) 
+print(first_name)
+print(last_name_len)
+print(name_short)
+print(chant)
+print(good_chant)
